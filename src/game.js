a7e4064b1a441e7ccd2a1229e63b6ec0bd3a2d0f
Michael McClimon <michael@mcclimon.org>
2022-08-05 19:54:09 -0400

    Improve list narrowing algorithm
    This better deals with guesses with multiple letters; it's a total
    rewrite of the narrowing algo.

--- src/game.js
+++ src/game.js
@@ -6,6 +6,8 @@ const randomElement = arr => arr[Math.floor(Math.random() * arr.length)];
 
 const isWin = results => results.every(el => el === CORRECT);
 
+const letterCount = (word, char) => word.split('').filter(c => c === char).length;
+
 const printGuess = (guess, results) => {
   let s = '';
 
@@ -64,43 +66,44 @@ const Game = class {
   }
 
   #filterWordList (guess, results) {
-    const letters = new Map();
-
-    guess.split('').forEach((char, idx) => {
-      letters.set(char, Math.max(letters.get(char) ?? results[idx], results[idx]));
-    });
-
+    const guessChars = guess.split('');
     let words = this.wordlist;
 
-    for (const [char, result] of letters) {
-      let idx;
-      switch (result) {
-        case WRONG:
-          words = words.filter(w => !w.includes(char));
-          break;
-        case MISPLACED:
-          // improve this
-          idx = [0, 1, 2, 3, 4].find(
-            i => guess.charAt(i) === char && results[i] === MISPLACED,
-          );
-          words = words.filter(w => w.charAt(idx) !== char);
-          break;
-        case CORRECT:
-          idx = [0, 1, 2, 3, 4].find(
-            i => guess.charAt(i) === char && results[i] === CORRECT,
-          );
-          words = words.filter(w => w.charAt(idx) === char);
+    const charCounts = new Map();
+    const inWordCounts = new Map();
+
+    // grab all the correct letters
+    guessChars.forEach((char, idx) => {
+      charCounts.set(char, (charCounts.get(char) || 0) + 1);
+
+      if (results[idx] > 0) {
+        inWordCounts.set(char, (inWordCounts.get(char) || 0) + 1);
+      }
+
+      if (results[idx] === CORRECT) {
+        words = words.filter(w => w.charAt(idx) === char);
+      }
+    });
+
+    // Our guess is LEVEE. We know that at most 1 E is correct, so we can
+    // remove words with > 1 E
+    for (const [char, count] of charCounts) {
+      const inWord = inWordCounts.get(char) || 0;
+
+      if (count > inWord) {
+        words = words.filter(w => letterCount(w, char) <= inWord);
       }
     }
 
-    guess.split('').forEach((char, idx) => {
-      if (results[idx] === WRONG) {
-        // console.log(`removing all words with ${char} at i=${idx}`);
-        words = words.filter(w => w.charAt(idx) !== char);
+    // we need to do something with our misplaced letters; this does more work
+    // than is strictly necessary
+    guessChars.forEach((char, idx) => {
+      if (results[idx] === MISPLACED) {
+        words = words.filter(w => letterCount(w, char) > 0 && w.charAt(idx) !== char);
       }
     });
 
-    return words.filter(w => w !== guess);
+    return words;
   }
 
   #checkGuess (guess) {
@@ -120,7 +123,6 @@ const Game = class {
 
     // here, all the correct letters are marked, and we have some letters that
     // aren't in the word at all, and some that are in the wrong place
-    // console.log({ guessChars, targetChars, results });
 
     guessChars.forEach((char, idx) => {
       if (char === null) {
